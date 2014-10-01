ca52e0e8d30c20304288fcbff292d20dd42fe694
Tim Holum <tim.holum@ixonos.com>
2014-10-01 13:54:43 -0500

    Adding index.html and modifying server for www

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-app.use(express.static(__dirname ) );
+app.use(express.static(__dirname + '/www' ) );
 var world = require('./world.js');
 world.init();
 var games = {demo : [] };
