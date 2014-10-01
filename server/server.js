eb7f540206dfb49e6c7a0c5f8f501206c22babba
Tim Holum <tim.holum@ixonos.com>
2014-10-01 08:47:19 -0500

    Adding A base frame from my chess game

---
+++ server/server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.use(express.static(__dirname ) );
+
+var games = {demo : [] };
+var history = {};
+
+io.on('connection', function (socket) {
+    socket.on('move', function ( move ) {
+        var game = move.game;
+        var from = move.from;
+        var to = move.to;
+        games[game].push({ from : from , to : to });
+        console.log( games );
+        io.sockets.emit('game' + game , { from : from , to : to } );
+    });
+    socket.on('joinGame' , function(game){
+        for( id in games[game]  ){
+            socket.emit('game' + game , games[game][id] );
+        }
+        
+    });
+    socket.on('listGames' , function(){
+        for( game in games ){
+            socket.emit('avalibleGames' , game);
+        }
+    });
+    socket.on('createGame' , function(name){
+       games[name] = []; 
+       io.sockets.emit('avalibleGames' , { add : name });
+    });
+    socket.on('resetGame' , function(game){
+        games[game] = [];
+        io.sockets.emit('game' , {reset : true });
+    });
+    socket.on('chat' , function(chat){
+        io.sockets.emit('chat' , chat );
+    });
+});
+
+server.listen(8080);
+
