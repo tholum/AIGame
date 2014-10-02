6c23dae3dff4da99ee7295545034b06526b2c0b8
Tim Holum <tim.holum@ixonos.com>
2014-10-01 20:01:34 -0500

    Starting Unit class

--- server/units.js
+++ server/units.js
@@ -1,3 +1,15 @@
 module.exports = {
-	init : function(){}
+	init : function(){},
+	createUnit : function( type ){
+		var c = this;
+		unit = function(t){
+			
+		}
+		return c( type );
+	},
+	unitTypes : {
+		'grunt' : { hp : 20 , speed : 1 , range : 1 , damage : 10 },
+		'archer': { hp : 10 , speed : 1 , range : 2 , damage : 10 },
+		'base'	: { hp : 200 , speed : 0 , range : 0 , damage : 0 }
+	}
 }
