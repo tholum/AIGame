7edadabab723e3152a170c61ab8ba0bcacf1c5c5
tholum <tholum@couleetechlink.com>
2018-03-09 21:55:00 -0600

    Fix A bunch of bugs

--- server/server.js
+++ server/server.js
@@ -1,145 +1,194 @@
-var obs = require('obs');
-var express = require('express');
+var obs = require("obs");
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-app.use(express.static(__dirname + '/www' ) );
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+app.use(express.static(__dirname + "/www"));
 
-var world = require('./world.js');
-var units = require('./units.js');
+var world = require("./world.js");
+var units = require("./units.js");
 
 var game = {
-    players : { 1 : {name: '' , gold : 100  } , 2 : {name: '' , gold : 100 } },
-    turn : 0,
-	player : 1,
-    winner : 0,
-    active : true,
-    phase : "",
-    phases : [ "move","attack","build" ],
-    phaseEnd : [],
-
+  players: { 1: { name: "", gold: 100 }, 2: { name: "", gold: 100 } },
+  turn: 0,
+  player: 1,
+  winner: 0,
+  active: true,
+  phase: "",
+  phases: ["move", "attack", "build"],
+  phaseEnd: []
 };
-game.endPhase = function(){
-    var self = game;
-    if( self.phase == self.phases[ self.phases.length -1 ] ){
-        self.turn++;
+game.endPhase = function() {
+  var self = game;
+  if (self.phase == self.phases[self.phases.length - 1]) {
+    self.turn++;
 
-        for( var u in units.units ){
-            units.units[u].newTurn();
-
-        }
-        self.phase = self.phases[0];
-    } else {
-        self.phase = self.phases[ self.phases.indexOf( self.phase ) + 1 ];
+    for (var u in units.units) {
+      units.units[u].newTurn();
     }
-    game.player = ((game.turn % 2) +1 );
-    for( var x in self.phaseEnd ){
-        self.phaseEnd[x]();
-    }
-}
+    self.phase = self.phases[0];
+  } else {
+    self.phase = self.phases[self.phases.indexOf(self.phase) + 1];
+  }
+  game.player = game.turn % 2 + 1;
+  for (var x in self.phaseEnd) {
+    self.phaseEnd[x]();
+  }
+};
 //Setting the game to the first phase
 game.phase = game.phases[0];
 
-game.phaseEnd.push( function(){
-    world.executeOn( 'phase' , game );
+game.phaseEnd.push(function() {
+  world.executeOn("phase", game);
 });
-world.on( 'action' , function( data ){
-    console.log('Action Ran');
-    console.log( data );
+world.on("action", function(data) {
+  console.log("Action Ran");
+  console.log(data);
 });
-world.on( 'error' , function( data ){
-    console.log('error');
-    console.log( data );
+world.on("error", function(data) {
+  console.log("error");
+  console.log(data);
 });
-world.on( 'phase' , function(){
-    console.log( 'phase ended');
+world.on("phase", function() {
+  console.log("phase ended");
 });
-world.on( 'phase' , function(){
-    if( game.phase == game.phases[0] ){
-        var player = ((game.turn % 2) +1 );
-        for( var u in units.units ){
-            var unit = units.units[u];
-            if( world.world.hasOwnProperty( unit.position ) ){
-            var tileClass = world.world[unit.position].tileClass;
-                if( unit.player == player && unit.collector == true && tileClass == "gold" ){
-                        game.players[player].gold = parseInt( game.players[player].gold ) + 10 ;
-                }
-            }
+world.on("phase", function() {
+  if (game.phase == game.phases[0]) {
+    var player = game.turn % 2 + 1;
+    for (var u in units.units) {
+      var unit = units.units[u];
+      if (world.world.hasOwnProperty(unit.position)) {
+        var tileClass = world.world[unit.position].tileClass;
+        if (
+          unit.player == player &&
+          unit.collector == true &&
+          tileClass == "gold"
+        ) {
+          game.players[player].gold = parseInt(game.players[player].gold) + 10;
         }
+      }
     }
+  }
 });
-world.init(game , units );
+world.init(game, units);
 units.init(game);
-var games = {demo : [] };
+var games = { demo: [] };
 var history = {};
 
-io.on('connection', function (socket) {
-    socket.on('joinGame' , function(game){
-        for( id in games[game]  ){
-            socket.emit('game' + game , games[game][id] );
-        }
-    });
-    socket.on( 'action' , function( data ){ world.action(  data  );  });
-});
-io.sockets.on('connection' , function( socket ){
-    world.on('action' , function( data ){ socket.emit( 'action' , data ); });
-    world.on('win' , function( data ){  socket.emit( 'win' , data ); });
-    world.on('phase' , function( ){  socket.emit( 'phase' , { game : game , units : units }); } );
-	socket.on( 'map' , function(data){
-		if( typeof data == "string" && world.world.hasOwnProperty( data )  ){
-			socket.emit( 'map', world.world[data] );
-		} else {
-			socket.emit( 'map', world.world );
-		}
-	} );
-  socket.on("getphase" , function(){
-    socket.emit( 'phase' , { game : game , units : units });
+io.on("connection", function(socket) {
+  socket.on("joinGame", function(game) {
+    for (id in games[game]) {
+      socket.emit("game" + game, games[game][id]);
+    }
   });
-	socket.on( 'game' , function(data){
-		socket.emit( 'game' , game );
-	});
-	socket.on( 'units' , function( data ){
-		socket.emit( 'units' , units.units );
-	});
+  socket.on("action", function(data) {
+    world.action(data);
+  });
+});
+io.sockets.on("connection", function(socket) {
+  world.on("action", function(data) {
+    socket.emit("action", data);
+  });
+  world.on("win", function(data) {
+    socket.emit("win", data);
+  });
+  world.on("phase", function() {
+    socket.emit("phase", { game: game, units: units });
+  });
+  socket.on("map", function(data) {
+    if (typeof data == "string" && world.world.hasOwnProperty(data)) {
+      socket.emit("map", world.world[data]);
+    } else {
+      socket.emit("map", world.world);
+    }
+  });
+  socket.on("getphase", function() {
+    socket.emit("phase", { game: game, units: units });
+  });
+  socket.on("game", function(data) {
+    socket.emit("game", game);
+  });
+  socket.on("units", function(data) {
+    socket.emit("units", units.units);
+  });
+});
+app.use((req, res, next) => {
+  let match = req.originalUrl.match(/\/([a-z]*)/);
+  let api = match !== null ? match[1] : "none";
 
-});
-app.get('/buildUnit', function( req , res ){
-    var unit = req.param('unit');
-    var to = req.param('to')
-    var type = req.param('type');
-    res.send( world.action( { action: 'build' , type : type ,unit : unit ,to : to }) );
-});
-app.get( '/action' , function( req , res ){
-    var query = require('url').parse(req.url,true).query;
-
-});
-app.get('/game' , function( req, res ){
-    res.send( game );
-});
-app.get('/map', function(req,res){
-	res.send( world.world );
-});
-app.get('/units', function(req,res){
-	res.send( units.units );
-});
-app.get('/move' ,  function(req,res){
-    var unit = req.param('unit');
-    var to = req.param('to');
-    res.send(  world.action( { action : "move" , unit : unit , to : to } ));
-});
-app.get('/attack' , function(req,res){
-    res.send( world.action(  require('url').parse(req.url,true).query ) );
-});
-app.get('/endphase' , function(req,res){
-    res.send( world.action( { action : "endphase" } ) );
+  let player = req.param("player");
+  if (
+    game.player == player ||
+    ["game", "map", "units", "none"].indexOf(api) !== -1
+  ) {
+    next();
+  } else {
+    res.send({
+      api,
+      error: `Player ${player} not allowed to do action ${api} for ${
+        game.player
+      }`
+    });
+  }
 });
 
-var base1 = units.createUnit( 'base' );
+app.get("/game", function(req, res) {
+  res.send(game);
+});
+app.get("/map", function(req, res) {
+  res.send(world.world);
+});
+
+app.get("/buildUnit", function(req, res) {
+  var unit = req.param("unit");
+  var to = req.param("to");
+  var type = req.param("type");
+  res.send(world.action({ action: "build", type: type, unit: unit, to: to }));
+});
+app.get("/action", function(req, res) {
+  var query = require("url").parse(req.url, true).query;
+});
+
+app.get("/units", function(req, res) {
+  let cleanUnits = {};
+  for (let key of Object.keys(units.units)) {
+    if (units.units[key].alive === true) {
+      cleanUnits[key] = units.units[key];
+    }
+  }
+  res.send(cleanUnits);
+});
+app.get("/move", function(req, res) {
+  var unit = req.param("unit");
+  var to = req.param("to");
+  res.send(world.action({ action: "move", unit: unit, to: to }));
+});
+app.get("/attack", function(req, res) {
+  let tmp = require("url").parse(req.url, true).query;
+  tmp.action = "attack";
+  let result = world.action({
+    action: "attack",
+    target: tmp.target,
+    unit: tmp.unit
+  });
+  [1, 2].forEach(id => {
+    if (units.units[id].alive === false) {
+      game.winner = units.units[id].player === 1 ? 2 : 1;
+      game.active = false;
+    }
+  });
+  res.send(result);
+});
+app.get("/endphase", function(req, res) {
+  res.send(world.action({ action: "endphase" }));
+});
+
+var base1 = units.createUnit("base");
 base1.player = 1;
-world.addUnit( base1 , "10.25" );
+world.addUnit(base1, "10.25");
 
-var base2 = units.createUnit( 'base' );
+var base2 = units.createUnit("base");
 base2.player = 2;
-world.addUnit( base2 , "40.24" );
+world.addUnit(base2, "40.24");
 
 server.listen(8080);
--- server/world.js
+++ server/world.js
@@ -1,222 +1,290 @@
 module.exports = {
-	/* { tileClass : 'base' , connections : [] } */
-	world : {},
-    on : function( string , func ){
-        var self = this;
-        if( self.onObj.hasOwnProperty( string ) ){
-            self.onObj[string].push( func );
-        }
-    },
-    executeOn : function( string , data ){
-        var self = this;
-        if( self.onObj.hasOwnProperty( string ) ){
-            for( var f in self.onObj[string] ){
-                self.onObj[string][f](data);
-            }
-        }
-    },
-    action : function( data ){
-        var self = this;
-        //move attack endphase
-        if( typeof data == "object" ){
-            if( data.hasOwnProperty("action") ){
-                switch( data.action ){
-                    case "move":
-                        if( data.hasOwnProperty('unit') && data.hasOwnProperty('to') ){
-                            var status = self.moveUnit( self.units.units[parseInt(data.unit)] , data.to );
-                            self.executeOn("action" , status );
-                            return status;
-                        } else {
-                            self.executeOn("error" , { status : 'invalid paramiters move' , data : data } );
-                        }
+  /* { tileClass : 'base' , connections : [] } */
+  world: {},
+  on: function(string, func) {
+    var self = this;
+    if (self.onObj.hasOwnProperty(string)) {
+      self.onObj[string].push(func);
+    }
+  },
+  executeOn: function(string, data) {
+    var self = this;
+    if (self.onObj.hasOwnProperty(string)) {
+      for (var f in self.onObj[string]) {
+        self.onObj[string][f](data);
+      }
+    }
+  },
+  action: function(data) {
+    var self = this;
+    //move attack endphase
+    if (typeof data == "object" && data !== null) {
+      console.log(typeof data);
+      console.log(data);
 
-                    break;
-                    case "attack":
-                        if( data.hasOwnProperty('unit') && data.hasOwnProperty('target') ){
-                            var status = self.attachUnit( self.units.units[parseInt(unit)] , self.units.units[parseInt(target)] );
-                            self.executeOn("action" , status );
-                        } else {
-                            self.executeOn("error" , { status : 'invalid paramiters attack' , data : data } );
-                            return status;
-                        }
-                    break;
-                    case "endphase":
-                        self.game.endPhase();
-                        self.executeOn("action" , self.game );
-                        return self.game;
-                    break;
-                    case "build":
-                        if( data.hasOwnProperty('type') && data.hasOwnProperty('unit') && data.hasOwnProperty('to') ){
-                            if( self.units.units.hasOwnProperty( data.unit ) ){
-
-															var unit = self.buildUnit(data.type,self.units.units[data.unit],data.to)
-                                self.executeOn("action" , unit );
-                            } else {
-
-                                self.executeOn("error" , { status : 'invalid unit' , data : data } );
-                            }
-                        } else {
-
-                            self.executeOn("error" , { status : 'invalid paramiters build' , data : data } );
-                            return status;
-                        }
-                    break;
-
-                }
-            }
-        }
-    },
-    onObj : {
-        "action" : [],
-        "phase" : [],
-        "win" : [],
-        "error" : []
-    },
-	tileType : {
-		'grass' : { speed : 1 , gold : false },
-		'gold' : { speed : 1 , gold : true }
-	},
-	validateMovement : function( data ){
-		var self = this;
-		if( data.hasOwnProperty("moves") && data.hasOwnProperty("from") && data.hasOwnProperty("to") ){
-			if( self.world.hasOwnProperty( data.from ) && self.world.hasOwnProperty( data.to ) ){
-				if( self.tileType[self.world[data.from].tileClass].speed <= data.moves && self.world[data.from].connections.indexOf(data.to) !== -1 ){
-					return { success : true , moves : (data.moves - self.tileType[self.world[data.from].tileClass].speed  ) }
-				} else {
-					return { success : false , errors : { speed : self.tileType[self.world[data.from].tileClass].speed <= data.moves , connection : self.world[data.from].connections.indexOf(data.to) !== -1  } };
-				}
-			} else {
-				return { success : false };
-			}
-		} else {
-			return { success : false };
-		}
-
-	},
-    getRange: function( from , to ){
-        var self = this;
-        if( self.world.hasOwnProperty( from ) && self.world.hasOwnProperty( to )){
-            var f = self.world[from];
-            var t = self.world[to];
-            return  Math.sqrt( Math.pow(f.x - t.x , 2) + Math.pow(f.y - t.y , 2) );
-
-        } else { return false; }
-    },
-
-    validateRange: function( range , from , to ){
-        var self = this;
-        var trueRange = self.getRange( from , to );
-        return range >= trueRange && trueRange !== false;
-    },
-    attackUnit: function( unit , target ){
-        var self = this;
-        if( self.game.phase == "attack" ){
-            var inRange = self.validateRange( unit.range , unit.position , target.position );
-            if( inRange ){
-                return unit.attack( self.getRange( unit.position, target.position) , target )
+      if (data.hasOwnProperty("action")) {
+        switch (data.action) {
+          case "move":
+            if (data.hasOwnProperty("unit") && data.hasOwnProperty("to")) {
+              var status = self.moveUnit(
+                self.units.units[parseInt(data.unit)],
+                data.to
+              );
+              self.executeOn("action", status);
+              return status;
             } else {
-                return false;
+              self.executeOn("error", {
+                status: "invalid paramiters move",
+                data: data
+              });
             }
+
+            break;
+          case "attack":
+            if (data.hasOwnProperty("unit") && data.hasOwnProperty("target")) {
+              var status = self.attackUnit(
+                self.units.units[parseInt(data.unit)],
+                self.units.units[parseInt(data.target)]
+              );
+              self.executeOn("action", status);
+            } else {
+              self.executeOn("error", {
+                status: "invalid paramiters attack",
+                data: data
+              });
+              return status;
+            }
+            break;
+          case "endphase":
+            self.game.endPhase();
+            self.executeOn("action", self.game);
+            return self.game;
+            break;
+          case "build":
+            if (
+              data.hasOwnProperty("type") &&
+              data.hasOwnProperty("unit") &&
+              data.hasOwnProperty("to")
+            ) {
+              if (self.units.units.hasOwnProperty(data.unit)) {
+                var unit = self.buildUnit(
+                  data.type,
+                  self.units.units[data.unit],
+                  data.to
+                );
+                self.executeOn("action", unit);
+              } else {
+                self.executeOn("error", { status: "invalid unit", data: data });
+              }
+            } else {
+              self.executeOn("error", {
+                status: "invalid paramiters build",
+                data: data
+              });
+              return status;
+            }
+            break;
+        }
+      }
+    }
+  },
+  onObj: {
+    action: [],
+    phase: [],
+    win: [],
+    error: []
+  },
+  tileType: {
+    grass: { speed: 1, gold: false },
+    gold: { speed: 1, gold: true }
+  },
+  validateMovement: function(data) {
+    var self = this;
+    if (
+      data.hasOwnProperty("moves") &&
+      data.hasOwnProperty("from") &&
+      data.hasOwnProperty("to")
+    ) {
+      if (
+        self.world.hasOwnProperty(data.from) &&
+        self.world.hasOwnProperty(data.to)
+      ) {
+        if (
+          self.tileType[self.world[data.from].tileClass].speed <= data.moves &&
+          self.world[data.from].connections.indexOf(data.to) !== -1 &&
+          self.world[data.to].units.length === 0
+        ) {
+          return {
+            success: true,
+            moves:
+              data.moves - self.tileType[self.world[data.from].tileClass].speed
+          };
         } else {
-            return false;
+          return {
+            success: false,
+            errors: {
+              speed:
+                self.tileType[self.world[data.from].tileClass].speed <=
+                data.moves,
+              connection:
+                self.world[data.from].connections.indexOf(data.to) !== -1
+            }
+          };
         }
-    },
-	moveUnit : function(unit , to ){
-		var self = this;
-        if( self.game.phase == "move" ){
-		    var vm = self.validateMovement({ moves : unit.movesLeft , from : unit.position , to : to });
-				console.log( vm );
-		    var playerTurn  = ((self.game.turn % 2) +1 ) == unit.player;
-		    if( vm.success === true && playerTurn ){
-			    self.removeUnit( unit  );
-			    unit.movesLeft = vm.moves;
-			    self.addUnit( unit , to );
-			    return { success : true };
-		    } else {
-			    return { success : false , error : { moveValid : vm.success , playerTurn : playerTurn } };
-		    }
+      } else {
+        return { success: false };
+      }
+    } else {
+      return { success: false };
+    }
+  },
+  getRange: function(from, to) {
+    var self = this;
+    if (self.world.hasOwnProperty(from) && self.world.hasOwnProperty(to)) {
+      var f = self.world[from];
+      var t = self.world[to];
+      return Math.sqrt(Math.pow(f.x - t.x, 2) + Math.pow(f.y - t.y, 2));
+    } else {
+      return false;
+    }
+  },
+
+  validateRange: function(range, from, to) {
+    var self = this;
+    var trueRange = self.getRange(from, to);
+    return range >= trueRange && trueRange !== false;
+  },
+  attackUnit: function(unit, target) {
+    var self = this;
+    if (self.game.phase == "attack") {
+      var inRange = self.validateRange(
+        unit.range,
+        unit.position,
+        target.position
+      );
+      if (inRange) {
+        return unit.attack(
+          self.getRange(unit.position, target.position),
+          target
+        );
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  },
+  moveUnit: function(unit, to) {
+    var self = this;
+    if (self.game.phase == "move") {
+      var vm = self.validateMovement({
+        moves: unit.movesLeft,
+        from: unit.position,
+        to: to
+      });
+      console.log(vm);
+      var playerTurn = self.game.turn % 2 + 1 == unit.player;
+      if (vm.success === true && playerTurn) {
+        self.removeUnit(unit);
+        unit.movesLeft = vm.moves;
+        self.addUnit(unit, to);
+        return { success: true };
+      } else {
+        return {
+          success: false,
+          error: { moveValid: vm.success, playerTurn: playerTurn }
+        };
+      }
+    }
+  },
+  buildUnit: function(type, unit, to) {
+    console.log("TUT;" + type + "," + unit + ",X:" + to.x + "Y:" + to.y);
+    var self = this;
+    if (
+      self.game.phase == "build" &&
+      self.world[unit.position].connections.indexOf(to) !== -1 &&
+      unit.canBuild.indexOf(type) !== -1 &&
+      self.units.unitTypes[type].cost <= self.game.players[unit.player].gold
+    ) {
+      var newunit = self.units.createUnit(type);
+      newunit.player = parseInt(unit.player);
+      self.game.players[unit.player].gold =
+        parseInt(self.game.players[unit.player].gold) -
+        (parseInt(newunit.cost) == NaN ? 0 : parseInt(newunit.cost));
+      return self.addUnit(newunit, to);
+    } else {
+      console.log("Unit Failed ");
+      return false;
+    }
+  },
+  addUnit: function(unit, to) {
+    var self = this;
+    unit.position = to;
+    self.world[unit.position].units.push(unit.id);
+    return true;
+  },
+  removeUnit: function(unit) {
+    var self = this;
+    var index = self.world[unit.position].units.indexOf(unit.id);
+    if (index > -1) {
+      self.world[unit.position].units.splice(index, 1);
+    }
+  },
+  generateWorld: function() {
+    var self = this;
+    var width = 50;
+    var height = 50;
+    var x = 1;
+    var y = 1;
+
+    while (x <= width) {
+      while (y <= height) {
+        var name = x + "." + y;
+        self.world[name] = {
+          name: name,
+          tileClass: "grass",
+          connections: [],
+          x: x,
+          y: y,
+          units: []
+        };
+        if (y < 50) {
+          self.world[name].connections.push(x + "." + (y + 1));
         }
-	},
-    buildUnit : function(type,unit,to){
-			console.log( "TUT;" + type + "," + unit + ",X:" + to.x + "Y:" + to.y );
-        var self = this;
-        if( self.game.phase == "build" &&
-            self.world[ unit.position ].connections.indexOf(to) !== -1 &&
-            unit.canBuild.indexOf( type ) !== -1 &&
-            self.units.unitTypes[type].cost <= self.game.players[unit.player].gold ){
-                var newunit = self.units.createUnit(type);
-                newunit.player = parseInt( unit.player );
-                self.game.players[unit.player].gold = parseInt( self.game.players[unit.player].gold )  - (parseInt(newunit.cost)==NaN?0:parseInt( newunit.cost ));
-                return self.addUnit( newunit , to );
-        } else {
-						console.log( "Unit Failed ");
-            return false;
+        if (y != 1) {
+          self.world[name].connections.push(x + "." + (y - 1));
         }
-    },
-	addUnit : function( unit , to ){
-		var self = this;
-		unit.position = to;
-		self.world[unit.position].units.push( unit.id );
-        return true;
-	},
-	removeUnit : function( unit ){
-		var self = this;
-		var index = self.world[unit.position].units.indexOf( unit.id );
-		if( index > -1 ){
-			self.world[unit.position].units.splice( index , 1 );
-		}
-	},
-	generateWorld : function(){
-		var self = this;
-		var width = 50;
-		var height = 50;
-		var x = 1;
-		var y = 1;
-
-		while( x <= width ){
-			while( y <= height ){
-				var name =  x + '.' + y;
-				self.world[ name ] = { name : name , tileClass : 'grass' , connections : [] , x : x , y : y , units : [] };
-				if(  y < 50 ){
-					self.world[ name ].connections.push( x + "." + (y+1) );
-				}
-				if(  y != 1 ){
-					self.world[ name ].connections.push( x + "." + (y-1) );
-				}
-				if( x < 50  ){
-					self.world[ name ].connections.push( (x+1) + "." + y );
-				}
-				if( x  != 1 ){
-					self.world[ name ].connections.push( (x-1) + "." + y );
-				}
-				y++;
-			}
-			y=1;
-			x++;
-		}
-		var list = [
-			"10.10",
-			"10.40",
-			"20.20",
-			"30.20",
-			"40.10",
-			"40.40",
-			"20.30",
-			"30.30"
-		]
-		for( var s in list ){
-			self.world[ list[s] ].tileClass = "gold";
-		}
-
-	},
-	game : {},
-    units : {},
-	init : function(game,units){
-		var self = this;
-		self.game = game;
-        self.units = units;
-		self.generateWorld();
-
-
-	}
-}
+        if (x < 50) {
+          self.world[name].connections.push(x + 1 + "." + y);
+        }
+        if (x != 1) {
+          self.world[name].connections.push(x - 1 + "." + y);
+        }
+        y++;
+      }
+      y = 1;
+      x++;
+    }
+    var list = [
+      "10.10",
+      "10.40",
+      "20.20",
+      "30.20",
+      "40.10",
+      "40.40",
+      "20.30",
+      "30.30"
+    ];
+    for (var s in list) {
+      self.world[list[s]].tileClass = "gold";
+    }
+  },
+  game: {},
+  units: {},
+  init: function(game, units) {
+    var self = this;
+    self.game = game;
+    self.units = units;
+    self.generateWorld();
+  }
+};
